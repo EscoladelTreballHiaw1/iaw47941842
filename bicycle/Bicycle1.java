1a1e5a4f06cafced491bbad98d0dcc8440ac4a52
Jordi Capellades Edo <jordiedo29@gmail.com>
2016-03-04 13:43:58 +0100

    Update Bicycle1.java

--- bicycle/Bicycle1.java
+++ bicycle/Bicycle1.java
@@ -1,7 +1,7 @@
 /*
  * Bicycle.java
  * 
- * Copyright 2010-2012 Mònica Ramírez Arceda <mramirez@escoladeltreball.org>
+ * Copyright 2010-2012 Jordi Capellades EDo <jordiedo29@gmail.com>
  * 
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
